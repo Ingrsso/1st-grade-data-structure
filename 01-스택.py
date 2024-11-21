cf5d478ed4c0d699b5de7533d07bcd25f6b8c939
SangEun Lee <dev.max@icloud.com>
2024-11-21 12:22:13 +0900

    Add: 2024.11.20 ~ 2024.11.21

---
+++ 01-스택.py
@@ -0,0 +1,39 @@
+class Stack:
+	def __init__(self):
+		self.items = []
+		
+	def is_empty(self):
+		return len(self.items) == 0
+	
+	def push(self, item):
+		self.items.append(item)
+	
+	def pop(self):
+		if (not self.is_empty()):
+			return self.items.pop()
+		else:
+			return None
+	
+	def peek(self):
+		if (not self.is_empty()):
+			return self.items[-1]
+		else:
+			return None
+	
+	def size(self):
+		return len(self.items)
+
+stack = Stack()
+
+stack.push(1)
+stack.push(2)
+stack.push(3)
+
+print("스택 크기:", stack.size())
+print("스택 최상단 요소:", stack.peek())
+print("스택에서 꺼낸 요소:", stack.pop())
+print("스택 크기:", stack.size())
+print("스택이 비어 있는가:", stack.is_empty())
+
+
+
